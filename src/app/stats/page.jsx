002ab086016d2158eba8e455e9c133f3dbf62c23
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-13 22:28:58 +0600

    create app route stats and timeline

---
+++ src/app/stats/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const statsPage = () => {
+  return (
+    <div>statsPage</div>
+  )
+}
+
+export default statsPage
---
+++ src/app/timeline/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const timeLinePage = () => {
+  return (
+    <div>timeLinePage</div>
+  )
+}
+
+export default timeLinePage
