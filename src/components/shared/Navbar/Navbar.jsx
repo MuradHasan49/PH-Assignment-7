7fb98de36d4753d3ee9424b09a319169b2de803f
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-13 21:13:45 +0600

    create navbar

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+"use client"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { IoHome, IoTimeOutline } from "react-icons/io5"
+import { MdOutlineQueryStats } from "react-icons/md"
+
+const Navbar = () => {
+    const path = usePathname()
+
+    return (
+        <>
+            <div className=" bg-white  shadow-sm">
+                <div className="navbar container mx-auto">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn bg-base-100 lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                            </div>
+                            <div
+                                tabIndex="-1"
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                    <div className="flex flex-col items-center justify-center gap-2">
+
+                                <Link href={"/home"}>
+                                    <button className={`${path == "/home" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline w-30`}><IoHome /> Home</button>
+                                </Link>
+                                <Link href={"/timeline "}>
+                                    <button className={`${path == "/timeline" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline  w-30`}><IoTimeOutline /> Timeline</button>
+                                </Link>
+                                <Link href={"/stats"}>
+                                    <button className={`${path == "/stats" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline  w-30`}><MdOutlineQueryStats />Stats</button>
+                                </Link>
+                                    </div>
+                            </div>
+                        </div>
+                        <Link href={"/"} className="text-black font-black text-2xl">Keen<span className="text-[#244D3F] font-semibold">Keeper</span></Link>
+                    </div>
+                    <div className="navbar-end hidden lg:flex gap-2 text-gray-600">
+                        <Link href={"/home"}>
+                            <button className={`${path == "/home" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline`}><IoHome /> Home</button>
+                        </Link>
+                        <Link href={"/timeline "}>
+                            <button className={`${path == "/timeline" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline`}><IoTimeOutline /> Timeline</button>
+                        </Link>
+                        <Link href={"/stats"}>
+                            <button className={`${path == "/stats" ? " bg-[#244D3F] text-white border-none" : ""} btn btn-outline`}><MdOutlineQueryStats />Stats</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
