f79d6f23ada68ef459b932bf408eff8ebff276ca
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-15 18:47:32 +0600

    fix responsive problem in stats section

--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -3,7 +3,6 @@ import React, { useContext } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
 import { RechartsDevtools } from '@recharts/devtools';
 import { InteractionsDataCotext } from '@/components/ContextApi/GlobalsContext';
-import NoDataFound from '@/components/shared/NoDataFound/NoDataFound';
 import NoFriendshipData from '@/components/shared/NoFriendshipData/NoFriendshipData';
 
 
@@ -21,35 +20,39 @@ const RechartPags = () => {
 
   return (
 
-    <div className="container mx-auto my-8 ">
-      <h2 className='text-3xl text-gray-800 font-bold py-10'>Friendship Analytics</h2>
-      <div className=" h-[50vh] border border-gray-600/50 rounded-lg">
-        <ResponsiveContainer width="100%" height="100%">
-          <PieChart>
-            <Pie
-              data={data}
-              cx="50%"
-              cy="50%"
-              innerRadius="70%"
-              outerRadius="90%"
-              paddingAngle={5}
-              cornerRadius={10}
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={entry.color} />
-              ))}
-            </Pie>
-            <Tooltip
-              contentStyle={{ borderRadius: '10px', border: 'none', boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1)' }}
-            />
-            <Legend />
+    <div className="container mx-auto my-8 px-2 lg:px-0">
+      {
+        interactionsData.length == 0 ? <NoFriendshipData /> :
+          <>
+            <h2 className='text-3xl text-gray-800 font-bold py-10'>Friendship Analytics</h2>
+            <div className=" h-[50vh] border border-gray-600/50 rounded-lg">
+              <ResponsiveContainer width="100%" height="100%">
+                <PieChart>
+                  <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    innerRadius="70%"
+                    outerRadius="90%"
+                    paddingAngle={5}
+                    cornerRadius={10}
+                    dataKey="value"
+                  >
+                    {data.map((entry, index) => (
+                      <Cell key={`cell-${index}`} fill={entry.color} />
+                    ))}
+                  </Pie>
+                  <Tooltip
+                    contentStyle={{ borderRadius: '10px', border: 'none', boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1)' }}
+                  />
+                  <Legend />
 
-            <RechartsDevtools />
-          </PieChart>
-        </ResponsiveContainer>
-      </div>
-      <NoFriendshipData/>
+                  <RechartsDevtools />
+                </PieChart>
+              </ResponsiveContainer>
+            </div>
+          </>
+      }
     </div>
 
   );
