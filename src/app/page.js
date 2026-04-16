ade3c93c41f388a841eedf4c68c70aaff16d1006
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-16 12:50:28 +0600

    added deploy link

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-[#f9fafb]">
       <div className="container mx-auto">
-        {/* <HomeContent /> */}
+        <HomeContent />
       </div>
     </div>
   );
