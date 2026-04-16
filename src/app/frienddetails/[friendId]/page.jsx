88084d327c67b149399047d80d8b5e09a17cc641
Murad Hasan <muradhasanshuvo00@gmail.com>
2026-04-16 13:04:41 +0600

    fix deploy porblem

--- src/app/frienddetails/[friendId]/page.jsx
+++ src/app/frienddetails/[friendId]/page.jsx
@@ -2,7 +2,7 @@ import ProfileDetails from "@/components/shared/ui/ProfileDetails/ProfileDetails
 
 const friendDetailsProfile = async ({ params }) => {
     const { friendId } = await params
-    const dataPromise = await fetch(`http://localhost:3000/apidata.json/`)
+    const dataPromise = await fetch("https://ph-assignment-7-gw8u.vercel.app/apidata.json")
     const data = await dataPromise.json();
 
     const specificUser = data.find(item => item.id == friendId)
